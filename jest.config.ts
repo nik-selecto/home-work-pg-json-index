45973e3937a39f9e794b85b61c33209005c00149
nik <nik.moiseienko@gmail.com>
2022-04-07 09:13:02 +0300

    feat: env

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@jest/types';
 
 export default async (): Promise<Config.InitialOptions> => ({
-    verbose: true,
-    detectOpenHandles: true,
-    detectLeaks: true,
+  verbose: true,
+  detectOpenHandles: true,
+  detectLeaks: true,
 });
