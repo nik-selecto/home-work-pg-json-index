610b7383da1211add9c3ec5d5755b66e4a1185db
nik <nik.moiseienko@gmail.com>
2022-04-18 10:14:42 +0300

    fix: rm dist from git

--- dist/components/app/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { ProjectConfigType } from '../../general-modules/config/config.type';
-export declare class AppService {
-    private configService;
-    constructor(configService: ConfigService<ProjectConfigType>);
-    getHello(): string;
-}
